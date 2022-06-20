f80a8a4a971aa93a3424a7525e32a260375a8b37
Shaul Amran <shaulamran@gmail.com>
2022-06-20 19:28:09 +0300

    Fixing bugs and adding unit tests

--- expression.js
+++ expression.js
@@ -1,95 +1,177 @@
-class Node {
-  constructor(data) {
-    this.data = data;
-    this.parent = null;
-    this.left = null;
-    this.right = null;
+class AtomicExpression {
+  constructor(operand1, operand2, obj) {
+    this.operand1 = operand1;
+    this.operand2 = operand2;
+    this.obj = obj;
   }
 }
 
-class Expression {
-  constructor(expression) {
-    this.calc = function calc(obj) {
-      var isOperatorRegExp = new RegExp(/^EQUAL\(|^AND\(|^OR\(/im);
-      let topTree;
-      let tree;
+class Equal extends AtomicExpression {
+  calc() {
+    return this.obj[this.operand1] == this.operand2;
+  }
+}
 
-      while (expression) {
-        const isOperator = isOperatorRegExp.test(expression);
+class GreaterThan extends AtomicExpression {
+  calc() {
+    return this.obj[this.operand1] > this.operand2;
+  }
+}
 
-        if (isOperator) {
-          const indexOfFirstParenthesis = expression.indexOf('(');
-          const operator = expression.substring(0, indexOfFirstParenthesis);
+class LowerThan extends AtomicExpression {
+  calc() {
+    return this.obj[this.operand1] < this.operand2;
+  }
+}
 
-          if (tree == undefined) {
-            tree = new Node(operator);
-            topTree = tree;
-          } else if (!tree.left) {
-            tree.left = new Node(operator);
-            tree.left.parent = tree;
-            tree = tree.left;
-          } else if (!tree.right) {
-            tree.right = new Node(operator);
-            tree.right.parent = tree;
-            tree = tree.right;
-          } else {
-            while (tree.right) {
-              tree = tree.parent;
-            }
-            tree.right = new Node(operator);
-            tree.right.parent = tree;
-            tree = tree.right;
-          }
+class ComplexExpression {
+  constructor(leftExpression, rightExpression) {
+    this.leftExpression = leftExpression;
+    this.rightExpression = rightExpression;
+  }
+}
 
-          expression = expression.substring(indexOfFirstParenthesis + 1);
-        } else {
-          var operandsRegex = new RegExp(/([^,]+),([^\)]+)\)?,?(.*)/gim);
-          var match = operandsRegex.exec(expression);
-          const operand1 = match[1];
-          const operand2 = match[2];
-          console.log(match);
-          expression = match[3];
-          expression = removeClosingParenthesis(expression);
-          tree.left = new Node(operand1);
-          tree.right = new Node(operand2);
-        }
-        console.log(expression);
+class AndOperator extends ComplexExpression {
+  calc() {
+    return this.leftExpression.calc() && this.rightExpression.calc();
+  }
+}
+
+class OrOperator extends ComplexExpression {
+  calc() {
+    return this.leftExpression.calc() || this.rightExpression.calc();
+  }
+}
+
+class NotOperator extends ComplexExpression {
+  calc() {
+    return !this.leftExpression.calc();
+  }
+}
+
+const operatorToClassMap = {
+  EQUAL: Equal,
+  GREATER_THAN: GreaterThan,
+  LOWER_THAN: LowerThan,
+  AND: AndOperator,
+  OR: OrOperator,
+  NOT: NotOperator,
+};
+
+function expressionToTree(expression, obj) {
+  expression = expression.replace(/\s/g, '').replace(/'/g, '');
+  var operator = extractOperator(expression);
+
+  if (operator == 'AND') {
+    const { expressionOne, expressionTwo } =
+      extractTwoSubExpressions(expression);
+    const expressionOneTree = expressionToTree(expressionOne, obj);
+    const expressionTwoTree = expressionToTree(expressionTwo, obj);
+    const andTree = new AndOperator(expressionOneTree, expressionTwoTree);
+    return andTree;
+  } else if (operator == 'OR') {
+    const { expressionOne, expressionTwo } =
+      extractTwoSubExpressions(expression);
+    const expressionOneTree = expressionToTree(expressionOne, obj);
+    const expressionTwoTree = expressionToTree(expressionTwo, obj);
+    const orTree = new OrOperator(expressionOneTree, expressionTwoTree);
+    return orTree;
+  } else if (operator == 'NOT') {
+    const subExpression = expression.substring(4, expression.length - 1);
+    const notTree = new NotOperator(expressionToTree(subExpression, obj));
+    return notTree;
+  } else if (
+    operator == 'EQUAL' ||
+    operator == 'GREATER_THAN' ||
+    operator == 'LOWER_THAN'
+  ) {
+    const { operand1, operand2 } = extractOperands(expression);
+    const operatorClass = operatorToClassMap[operator];
+    const expressionTree = new operatorClass(operand1, operand2, obj);
+    return expressionTree;
+  }
+}
+
+function extractTwoSubExpressions(expression) {
+  let parenthesisCounter = 0;
+  let i = 0;
+  let beginningOfFirstExpression = -1;
+  let endOfFirstExpression = -1;
+  let beginningOfSecondExpression = -1;
+  let endOfSecondExpression = -1;
+
+  let STAGES = {
+    FIRST_EXPRESSION_BEGINNING: 1,
+    FIRST_EXPRESSION_END: 2,
+    SECOND_EXPRESSION_BEGINNING: 3,
+    SECOND_EXPRESSION_END: 4,
+  };
+
+  let stage = STAGES.FIRST_EXPRESSION_BEGINNING;
+
+  while (i < expression.length) {
+    if (expression[i] == '(') {
+      parenthesisCounter++;
+
+      if (
+        stage == STAGES.FIRST_EXPRESSION_BEGINNING &&
+        parenthesisCounter == 1
+      ) {
+        beginningOfFirstExpression = i + 1;
+        stage = STAGES.FIRST_EXPRESSION_END;
       }
-      console.log("finished with expression");
+    }
 
-      return evalTree(topTree, obj);
-    };
+    if (expression[i] == ')') {
+      parenthesisCounter--;
+
+      if (stage == STAGES.FIRST_EXPRESSION_END && parenthesisCounter == 1) {
+        endOfFirstExpression = i + 1;
+        beginningOfSecondExpression = i + 2; // skipping comma between the two expressions
+        stage = STAGES.SECOND_EXPRESSION_END;
+      }
+
+      if (stage == STAGES.SECOND_EXPRESSION_END && parenthesisCounter == 1) {
+        endOfSecondExpression = i + 1;
+      }
+    }
+
+    i++;
   }
+  const expressionOne = expression.substring(
+    beginningOfFirstExpression,
+    endOfFirstExpression
+  );
+  const expressionTwo = expression.substring(
+    beginningOfSecondExpression,
+    endOfSecondExpression
+  );
 
+  return { expressionOne, expressionTwo };
 }
 
-function evalTree(tree, obj) {
-  if (tree.data == 'EQUAL') {
-    return obj[tree.left.data] == tree.right.data;
-  } else if (tree.data == 'OR') {
-    return evalTree(tree.left, obj) || evalTree(tree.right, obj);
-  } else if (tree.data == 'AND') {
-    return evalTree(tree.left, obj) && evalTree(tree.right, obj);
+function extractOperands(expression) {
+  var operandsRegex = new RegExp(/([^(]+),([^\)]+)\)/gim);
+  var match = operandsRegex.exec(expression);
+  const operand1 = match[1];
+  const operand2 = match[2];
+  return { operand1, operand2 };
+}
+
+function extractOperator(expression) {
+  if (expression.startsWith('AND')) {
+    return 'AND';
+  } else if (expression.startsWith('OR')) {
+    return 'OR';
+  } else if (expression.startsWith('NOT')) {
+    return 'NOT';
+  } else if (expression.startsWith('EQUAL')) {
+    return 'EQUAL';
+  } else if (expression.startsWith('GREATER_THAN')) {
+    return 'GREATER_THAN';
+  } else if ('LOWER_THAN') {
+    return 'LOWER_THAN';
   }
 }
 
-function removeClosingParenthesis(string) {
-  while (string.indexOf(')') === 0) {
-    string = string.substring(1, string.length);
-  }
-
-  return string;
-}
-
-
-//AND(EQUAL(id,"first-post"),EQUAL(views,100))
-var expression = new Expression(
-  "AND(EQUAL(id,first-post),EQUAL(views,100))"
-);
-
-var store = [
-  { id: 'first-post', title: 'alphabet', views: 100 },
-  //{ id: 'first-post', views: 200 },
-];
-
-console.log(store.filter((obj) => expression.calc(obj)));
+export default expressionToTree;
---
+++ expression.test.js
@@ -0,0 +1,94 @@
+import expressionToTree from './expression';
+
+test('Equal', () => {
+  generateTest({
+    EXPRESSION: 'EQUAL(id,first-post)',
+    TO_BE_FOUND: [{ id: 'first-post' }],
+    TO_NOT_BE_FOUND: [{ id: 'second-post' }],
+  });
+});
+
+test('Greater than', () => {
+  generateTest({
+    EXPRESSION: 'GREATER_THAN(views,6)',
+    TO_BE_FOUND: [{ views: 10 }],
+    TO_NOT_BE_FOUND: [{ views: 2 }],
+  });
+});
+
+test('Lower than', () => {
+  generateTest({
+    EXPRESSION: 'LOWER_THAN(views,6)',
+    TO_BE_FOUND: [{ views: 2 }],
+    TO_NOT_BE_FOUND: [{ views: 10 }],
+  });
+});
+
+test('AND', () => {
+  generateTest({
+    EXPRESSION: 'AND(EQUAL(id,first-post),EQUAL(views,100))',
+    TO_BE_FOUND: [{ id: 'first-post', views: 100 }],
+    TO_NOT_BE_FOUND: [{ id: 'first-post' }],
+  });
+});
+
+test('OR', () => {
+  generateTest({
+    EXPRESSION: 'OR(EQUAL(id,first-post),EQUAL(views,100))',
+    TO_BE_FOUND: [{ id: 'first-post' }, { views: 100 }],
+    TO_NOT_BE_FOUND: [{ id: 'second-post', views: 6 }],
+  });
+});
+
+test('NOT', () => {
+  generateTest({
+    EXPRESSION: 'NOT(EQUAL(id,second-post))',
+    TO_BE_FOUND: [{ id: 'first-post' }],
+    TO_NOT_BE_FOUND: [{ id: 'second-post' }],
+  });
+});
+
+test('Big complex on the left side', () => {
+  generateTest({
+    EXPRESSION:
+      'AND(AND(EQUAL(id,first-post),GREATER_THAN(likes,50)),EQUAL(views,100))',
+    TO_BE_FOUND: [{ id: 'first-post', views: 100, likes: 51 }],
+    TO_NOT_BE_FOUND: [{ id: 'first-post', views: 100, likes: 49 }],
+  });
+});
+
+test('Big complex on the right side', () => {
+  generateTest({
+    EXPRESSION:
+      'AND(EQUAL(views,100),AND(EQUAL(id,first-post),GREATER_THAN(likes,50)))',
+    TO_BE_FOUND: [{ id: 'first-post', views: 100, likes: 51 }],
+    TO_NOT_BE_FOUND: [{ id: 'first-post', views: 100, likes: 49 }],
+  });
+});
+
+test('Expression with spaces', () => {
+  generateTest({
+    EXPRESSION: 'EQUAL( id , first-post )',
+    TO_BE_FOUND: [{ id: 'first-post' }],
+    TO_NOT_BE_FOUND: [{ id: 'second-post' }],
+  });
+});
+
+test('Expression with single quotation marks', () => {
+  generateTest({
+    EXPRESSION: `EQUAL( id , 'first-post' )`,
+    TO_BE_FOUND: [{ id: 'first-post' }],
+    TO_NOT_BE_FOUND: [{ id: 'second-post' }],
+  });
+});
+
+function generateTest(data) {
+  const { EXPRESSION, TO_BE_FOUND, TO_NOT_BE_FOUND } = data;
+  const store = [...TO_BE_FOUND, ...TO_NOT_BE_FOUND];
+
+  const result = store.filter((obj) =>
+    expressionToTree(EXPRESSION, obj).calc()
+  );
+  expect(result).toEqual(expect.arrayContaining([...TO_BE_FOUND]));
+  expect(result).toEqual(expect.not.arrayContaining([...TO_NOT_BE_FOUND]));
+}
